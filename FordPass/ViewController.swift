ca87d399e6abfbb4bf43b486b424d51a032a6876
Leandro Luna <leandro.j.luna@gmail.com>
2022-10-11 01:27:27 -0300

    Final commit

--- FordPass/ViewController.swift
+++ FordPass/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func upgradeButton(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Em manutenção!", message: "Retornaremos o serviço em breve..", preferredStyle: .alert)
 
+        let OKAction = UIAlertAction(title: "Ok", style: .default) {
+            (action: UIAlertAction!) in
+        }
+        
+        alertController.addAction(OKAction)
+
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel) {
+            (action: UIAlertAction!) in
+        }
+        
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
